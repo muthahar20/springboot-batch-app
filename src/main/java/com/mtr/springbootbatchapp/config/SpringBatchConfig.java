ab28ee6a3eedc60a943dd1971d8dbd7730156626
muthahar20@gmail.com <muthahar20@gmail.com>
2023-12-05 17:52:24 -0500

    initial commit

---
+++ src/main/java/com/mtr/springbootbatchapp/config/SpringBatchConfig.java
@@ -0,0 +1,18 @@
+package com.mtr.springbootbatchapp.config;
+
+import com.mtr.springbootbatchapp.CustomerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+@AllArgsConstructor
+public class SpringBatchConfig {
+
+    private JobBuilderFactory jobBuilderFactory;
+    private StepBuilderFactory stepBuilderFactory;
+    private CustomerRepository customerRepository;
+}
---
+++ src/test/java/com/mtr/springbootbatchapp/SpringbootBatchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtr.springbootbatchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBatchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
